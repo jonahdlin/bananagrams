b27768b26db211276226d4babba6783cd91a681e
nadavhames <nddhames@gmail.com>
2018-01-07 16:18:33 -0500

    Conditional Rendering to switch between menu and game

--- src/features/game/App.js
+++ src/features/game/App.js
@@ -6,18 +6,23 @@ import BoardContainer from './BoardContainer';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
 
+    this.state = {
+      showGame: true //server controlled instead of showGame? 
     }
   }
 
   render() {
-    return (
-      <div className='App'>
-        <Header />
-        <BoardContainer />
+    var div;
+    if (this.state.showGame){
+      div = <div className='App'>
+      <Header />
+      <BoardContainer />
       </div>
-    );
+    } else {
+      div = <div className='Menu'> <h1>Menu features go here </h1> </div>
+    }
+    return div;
   }
 }
 
